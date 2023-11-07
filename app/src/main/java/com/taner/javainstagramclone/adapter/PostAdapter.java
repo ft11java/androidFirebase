bbd5dc491a2f451e4ea56643ad4a5512682149fa
ft11java <ft11java>
2023-11-07 16:45:23 +0300

    first commit

---
+++ app/src/main/java/com/taner/javainstagramclone/adapter/PostAdapter.java
@@ -0,0 +1,54 @@
+package com.taner.javainstagramclone.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+import com.taner.javainstagramclone.databinding.RecyclerRowBinding;
+import com.taner.javainstagramclone.model.Post;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostHolder> {
+
+    private ArrayList<Post> postArrayList;
+
+    public PostAdapter(ArrayList<Post> postArrayList) {
+        this.postArrayList = postArrayList;
+    }
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding=RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PostHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+       holder.recyclerRowBinding.recyclerViewUserEmailText.setText(postArrayList.get(position).email);
+       holder.recyclerRowBinding.recyclerViewCommentText.setText(postArrayList.get(position).comment);
+        Picasso.get().load(postArrayList.get(position).downloadUrl).into(holder.recyclerRowBinding.recyclerViewImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return postArrayList.size();
+    }
+
+    class PostHolder extends RecyclerView.ViewHolder{
+
+        RecyclerRowBinding recyclerRowBinding;
+
+        public PostHolder(@NonNull RecyclerRowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding=recyclerRowBinding;
+        }
+    }
+
+}
